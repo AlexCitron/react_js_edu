746557a09a0412866422615796cbe3083f0a9975
Alex V <avostr@mail.ru>
2025-08-01 18:16:21 +0300

    REF handlers added for onClick function

--- third_practice/src/components/navigation.container.jsx
+++ third_practice/src/components/navigation.container.jsx
@@ -1,10 +1,11 @@
 import Button from "./button.jsx";
+import {handlePreviousButtonClick, handleNextButtonClick} from "../handlers/handlers.jsx";
 
-export default function NavigationContainer() {
+export default function NavigationContainer({tabArrayNames, activeTabButton, setActiveTabButton}) {
     return (
         <div className='navigation-buttons'>
-            <Button name='< Previous' />
-            <Button name='Next >' />
+            <Button name='< Previous' onClick={() => handlePreviousButtonClick(tabArrayNames, activeTabButton, setActiveTabButton)} />
+            <Button name='Next >' onClick={() => handleNextButtonClick (tabArrayNames, activeTabButton, setActiveTabButton)} />
         </div>
     )
 }
