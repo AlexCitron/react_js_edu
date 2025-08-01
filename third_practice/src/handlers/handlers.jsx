c7bef907646fa71b993f1a015a5492a2fd125f95
Alex V <avostr@mail.ru>
2025-08-01 18:15:26 +0300

    REF Next and Previous buttons click handlers added

--- third_practice/src/handlers/handlers.jsx
+++ third_practice/src/handlers/handlers.jsx
@@ -6,4 +6,21 @@ function handleTabButtonClick(setActiveTabButton, name, activeTabButton) {
     (activeTabButton === name) ? setActiveTabButton(null) : setActiveTabButton(name);
 }
 
-export {handleStartButtonClick, handleTabButtonClick}
+function handlePreviousButtonClick(tabArrayNames, activeTabButton, setActiveTabButton) {
+    const current_pos = tabArrayNames.indexOf(activeTabButton)
+    if (!activeTabButton) {
+        }
+        else {
+            (current_pos - 1 >= 0) ? setActiveTabButton(tabArrayNames[current_pos - 1]) : setActiveTabButton(tabArrayNames[current_pos]);
+        }
+}
+function handleNextButtonClick(tabArrayNames, activeTabButton, setActiveTabButton) {
+    const current_pos = tabArrayNames.indexOf(activeTabButton)
+    if (!activeTabButton) {
+        }
+        else {
+            (current_pos + 1 < tabArrayNames.length) ? setActiveTabButton(tabArrayNames[current_pos + 1]) : setActiveTabButton(tabArrayNames[current_pos]);
+        }
+}
+
+export { handleStartButtonClick, handleTabButtonClick, handlePreviousButtonClick, handleNextButtonClick }
