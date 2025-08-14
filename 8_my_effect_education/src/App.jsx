05d612f3bf68e5bf06336bf6a3f21fa60a5b9bcb
Alex V <avostr@mail.ru>
2025-08-14 17:51:23 +0300

    REF getWeatherData async func added, API tested

--- 8_my_effect_education/src/App.jsx
+++ 8_my_effect_education/src/App.jsx
@@ -8,11 +8,18 @@ export default function App() {
     const [city, setCity] = useState("");
     const [weatherData, setWeatherData] = useState(null);
 
+    async function getWeatherData() {
+        const response = await fetch(
+            `http://api.weatherapi.com/v1/current.json?key=${KEY}&q=Taganrog`
+        );
+        const data = await response.json();
+        console.log(data);
+    }
 
-
-  return (
+    return (
     <div className={'app'}>
       <Main>
+          <button className={'button'} onClick={getWeatherData}></button>
           <h1 className={'app-title'}>Weather Widget</h1>
           <div className={'search-container'}>
               <input className={'search-input'} type='text' placeholder='Enter city name' />
@@ -32,5 +39,5 @@ export default function App() {
           </WeatherCard>
       </Main>
     </div>
-  )
+    )
 }
