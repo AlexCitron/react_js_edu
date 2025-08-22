a4444abda935eb58a6f8e9569d7e609229d22417
Alex V <avostr@mail.ru>
2025-08-22 08:16:03 +0300

    NEW pages created

--- 12_router/src/App.jsx
+++ 12_router/src/App.jsx
@@ -1,9 +1,21 @@
-import "index.css"
+import "./index.css"
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Cart from "./pages/Cart.jsx";
+import Categories from "./pages/Categories.jsx";
+import NotFound from "./pages/NotFound.jsx";
+import ProductDetails from "./pages/ProductDetails.jsx";
+
+const router = createBrowserRouter([
+  {path: "/", element: <Home/>},
+  {path: "about", element: <About/>},
+  {path: "cart", element: <Cart/>},
+  {path: "categories", element: <Categories/>},
+  {path: "product", element: <ProductDetails/>},
+  {path: "*", element: <NotFound/>},
+])
 
 export default function App() {
-
-  return (
-    <>
-    </>
-  )
+  return <RouterProvider router={router} />
 }
