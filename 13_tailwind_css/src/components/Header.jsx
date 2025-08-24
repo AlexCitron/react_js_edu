dcc4448f9d81274dccc47ef64b3964d57cc37dbb
Alex V <avostr@mail.ru>
2025-08-24 11:16:34 +0300

    REF shadow added

--- 13_tailwind_css/src/components/Header.jsx
+++ 13_tailwind_css/src/components/Header.jsx
@@ -3,7 +3,7 @@ import {NavLink} from "react-router-dom";
 export function Header({children}) {
 
     return (
-        <header className={'flex py-8 px-8 justify-between header bg-blue-200'}>
+        <header className={'flex py-8 px-8 justify-between header bg-blue-200 shadow-md'}>
             <img className={"h-6"} src={"/logo.svg"} alt={'logo'}/>
             <ul className={'flex gap-x-8'}>
                 <li><NavLink className={"font-semibold"} to={'/'}>Home</NavLink></li>
