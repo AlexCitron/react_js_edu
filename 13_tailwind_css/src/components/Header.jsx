dd8f41085645f0bb89fe9b04cfb380cac7b656f7
Alex V <avostr@mail.ru>
2025-08-24 14:09:03 +0300

    NavMenuLink component added, style applied thru tailwind, state cosidered

--- 13_tailwind_css/src/components/Header.jsx
+++ 13_tailwind_css/src/components/Header.jsx
@@ -1,14 +1,15 @@
 import {NavLink} from "react-router-dom";
+import NavMenuLink from "../UI/NavMenuLink.jsx";
 
 export function Header({children}) {
 
     return (
         <header className={'flex py-8 px-8 justify-between header bg-blue-200 shadow-md'}>
             <img className={"h-6"} src={"/logo.svg"} alt={'logo'}/>
-            <ul className={'flex gap-x-8'}>
-                <li><NavLink className={"links"} to={'/'}>Home</NavLink></li>
-                <li><NavLink className={"links"} to={'/about'}>About</NavLink></li>
-                <li><NavLink className={"links"} to={'/cart'}>Cart</NavLink></li>
+            <ul className={'flex gap-14'}>
+                <li><NavMenuLink to={'/'}>Home</NavMenuLink></li>
+                <li><NavMenuLink to={'/about'}>About</NavMenuLink></li>
+                <li><NavMenuLink to={'/cart'}>Cart</NavMenuLink></li>
             </ul>
         </header>
 )
