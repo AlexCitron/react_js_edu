6f7ba61743c1f2a9aebe80fb0bf73eac53d2872d
Alex V <avostr@mail.ru>
2025-08-25 17:38:41 +0300

    REF styles added to Category

--- 13_tailwind_css/src/pages/Category.jsx
+++ 13_tailwind_css/src/pages/Category.jsx
@@ -33,17 +33,45 @@ export default function Category() {
         return <p>Category not found</p>
     }
         return (
-            <div className={'CategoryContainer'}>
-                <div className="priceFilterContainer">
-                    <input id="minPrice" type="text" placeholder='0' value={minSearchPrice} onChange={updateSearchParams}/>
-                    <input id="maxPrice" type="text" placeholder={maxSearchPrice} value={maxSearchPrice} onChange={updateSearchParams}/>
+            <div className="py-10 px-6">
+                <h1 className="mb-4 text-3xl font-semibold text-center">Category {categoryId}</h1>
+                <div className="mb-4 ml-5 flex">
+                    <div className="mr-4">
+                        <label
+                            className="block text-gray-500 font-medium"
+                            htmlFor="minPrice">Min price</label>
+                        <input
+                            className="border border-gray-200 rounded-md w-20 placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+                            id="minPrice"
+                            type="text"
+                            placeholder='0'
+                            value={minSearchPrice}
+                            onChange={updateSearchParams}/>
+                    </div>
+                    <div>
+                        <label
+                            className="block text-gray-500 font-medium"
+                            htmlFor="maxPrice">Max price</label>
+                        <input
+                            className="border border-gray-200 rounded-md w-20 placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+                            id="maxPrice"
+                            type="text"
+                            placeholder={maxSearchPrice}
+                            value={maxSearchPrice}
+                            onChange={updateSearchParams}/>
+                    </div>
                 </div>
-                <ul style={{ display: "flex"}}>
+                <ul className="grid grid-cols-3 gap-4 px-5">
                     {filteredCategoryGoodsList.map((product) => (
-                        <li key={product.id} style={{ listStyle: "none" }}>
-                            <Link to={`/product/${product.id}`}>
-                                {product.name} {product.price}$
-                                {<img src={product.img} alt={product.name} style={{ width: "150px"}}/>}
+                        <li
+                            key={product.id}
+                            style={{ listStyle: "none" }}>
+                            <Link
+                                className="relative flex flex-col items-center justify-center group"
+                                to={`/product/${product.id}`}>
+                                <span className='absolute z-10 text-md font-semibold text-white group-hover:text-xl  duration-500 text-center'>{product.name}<br/>{product.price}$</span>
+                                {<img className="rounded-xl" src={product.img} alt={product.name}/>}
+                                <div className='absolute inset-0 rounded-xl group-hover:bg-gray-900 group-hover:opacity-70 transition duration-500'></div>
                             </Link>
                         </li>
                     ))}
