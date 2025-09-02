aaca5c8bd892da38eeb8036bbec937475675cd2b
Alex V <avostr@mail.ru>
2025-09-02 19:17:19 +0300

    done

--- 16_reducer_practice/src/AppContext.jsx
+++ 16_reducer_practice/src/AppContext.jsx
@@ -1,11 +1,39 @@
-import {createContext, useContext} from 'react'
+import {createContext, useContext, useReducer} from 'react'
 
 const MyAppContext = createContext({})
+const options = {
+    weekday: 'short',
+    month: 'short',
+    day: 'numeric',
+    year: 'numeric',
+}
+const initialValue = {date: new Date().toDateString(), inputValue: ""}
 
+function reducer(state = initialValue, action) {
+    switch (action.type) {
+        case "inputChanged":
+            return {...state, inputValue: action.payload}
+        case "calcDays":
+            const days = parseInt(action.payload, 10)
+            if(!isNaN(days)) {
+                let res = new Date();
+                res.setDate(res.getDate() + days)
+                return {...state, inputValue: "", date: res.toDateString()}
+            }
+            return {...state, inputValue: ""}
+        case "reset":
+            return {date: new Date().toDateString() , inputValue: ""}
+    }
+}
 
 export default function AppContext({children}){
+    const [state, dispatch] = useReducer(reducer, initialValue)
+
     return(
-        <MyAppContext.Provider value={{}}>
+        <MyAppContext.Provider value={{
+            state,
+            dispatch,
+        }}>
         {children}
         </MyAppContext.Provider>
     )
