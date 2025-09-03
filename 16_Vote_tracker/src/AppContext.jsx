39fc95020e416d0f64b054b7695ca640e846fbfb
Alex V <avostr@mail.ru>
2025-09-03 09:50:04 +0300

    REF, list of voters now remains in the same order as initial one, cannot redice votes less than zero

--- 16_Vote_tracker/src/AppContext.jsx
+++ 16_Vote_tracker/src/AppContext.jsx
@@ -16,11 +16,25 @@ function reducer(state, action) {
             }];
         return {candidates: newCandidates, inputValue: "" };
         case "addVote":
-            const filteredAddList = state.candidates.filter(({id}) => id !== action.payload.id);
-            return {...state, candidates: [...filteredAddList, { ...action.payload, votes: action.payload.votes + 1}] };
+            const addVoteList =  []
+            for ( let {id, name, votes} of state.candidates) {
+                if(id === action.payload.id) {
+                    addVoteList.push({id, name, votes: votes + 1});
+                } else {
+                    addVoteList.push({id, name, votes});
+                }
+            }
+            return {...state, candidates: addVoteList}
         case "removeVote":
-            const filteredRemoveList = state.candidates.filter(({id}) => id !== action.payload.id);
-            return {...state, candidates: [...filteredRemoveList, { ...action.payload, votes: action.payload.votes - 1}] };
+            const removeVoteList =  []
+            for ( let {id, name, votes} of state.candidates) {
+                if(id === action.payload.id) {
+                    removeVoteList.push({id, name, votes: votes ? votes - 1 : votes});
+                } else {
+                    removeVoteList.push({id, name, votes});
+                }
+            }
+            return {...state, candidates: removeVoteList };
         case "resetVotes":
             const resetList = state.candidates.map((p) => {return {...p, votes: 0}})
             return {...state, candidates: resetList};
