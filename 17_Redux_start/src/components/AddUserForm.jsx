c96d91333e0f976a3a09ac61ca43badf714f2144
Alex V <avostr@mail.ru>
2025-09-04 12:34:06 +0300

    NEW creted Input jsx, AddUserForm pre-styled

--- 17_Redux_start/src/components/AddUserForm.jsx
+++ 17_Redux_start/src/components/AddUserForm.jsx
@@ -1,9 +1,15 @@
+import Input from "./Input.jsx";
+
 export default function AddUserForm( ) {
     return (
-        <div className="border border-gray-200 mt-10 ">
-            <input placeholder='Name' type='text' />
-            <input placeholder='Email' type='email' />
-            <button>Add user</button>
+        <div className="mt-10 flex gap-3 justify-start ">
+            <Input placeholder='Name' type='text' />
+            <Input placeholder='Email' type='email' />
+            <button
+                className="bg-green-500 rounded-md w-1/10 text-white py-3 hover:bg-green-600
+                transition duration-500"
+                onClick={() => {console.log('click')}}
+            >Add user</button>
         </div>
     )
 }
---
+++ 17_Redux_start/src/components/Input.jsx
@@ -0,0 +1,13 @@
+export default function Input({placeholder, value, type, onChange}) {
+    return (
+        <>
+            <input
+                value={value}
+                className="bg-white border-1 border-gray-400 rounded-md px-5 w-9/20
+                focus:outline-3 focus:outline-blue-500 focus:border-0"
+                placeholder={ placeholder }
+                type={type}
+                onChange={onChange}/>
+        </>
+    )
+}
