6a2b473955027000dbceffa34e742e3a9f33604e
Alex V <avostr@mail.ru>
2025-09-07 10:33:26 +0300

    refactoring as RTK completed

--- 17_Redux_start/src/features/userList/UsersList.jsx
+++ 17_Redux_start/src/features/userList/UsersList.jsx
@@ -5,12 +5,15 @@ import {selectUser} from "../userDetails/userSliceDetails.js";
 
 export default function UsersList() {
 
+    /*The required states are fetched from the state storage*/
     const users = useSelector(state => state.userList.users);
     const loading = useSelector(state => state.userList.loading);
     const error = useSelector(state => state.userList.error);
 
+    /*dispatch*/
     const dispatch = useDispatch();
 
+    /*Handlers section*/
     function handleLoadUsers() {
         dispatch(fetchUsers());
     }
@@ -23,6 +26,7 @@ export default function UsersList() {
         dispatch(selectUser(id));
     }
 
+    /*Users list section*/
     return (
         <div className="my-5 w-full bg-white shadow rounded-md p-3 space-y-2">
             <h2 className="text-xl my-1 font-bold">User List</h2>
@@ -32,9 +36,13 @@ export default function UsersList() {
                 onClick={handleLoadUsers}
             >Load Users</button>
 
+            {/*show loading while loading*/}
             {loading && <p>Loading...</p>}
+
+            {/*show an error message if an error occurred*/}
             {error && <p><strong>Error:</strong> {error}</p>}
 
+            {/*Users list mapped*/}
             <ul className='mt-2 space-y-2'>
                 {users?.map(({id, name, email}) => (
                     <li key={id} className="flex justify-between">
